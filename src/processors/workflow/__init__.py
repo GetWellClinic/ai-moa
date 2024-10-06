2a95c3b283748496fd8c5e84af06f25dbded7777
Michael Yingbull <myingbull@conestogac.on.ca>
2024-10-06 10:11:12 -0400

    feat: Create workflow processor module

--- src/processors/workflow/__init__.py
+++ src/processors/workflow/__init__.py
@@ -1,3 +1,6 @@
 from .processor import WorkflowProcessor
 from .step_executor import WorkflowStepExecutor
 from .task_manager import WorkflowTaskManager
+from .processor import WorkflowProcessor
+from .step_executor import WorkflowStepExecutor
+from .task_manager import WorkflowTaskManager
