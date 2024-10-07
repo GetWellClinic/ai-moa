f429d787ebf52125caec2d963dadc8863ff35328
Michael Yingbull <myingbull@conestogac.on.ca>
2024-10-07 01:20:09 -0400

    fix: remove redundant __all__ declaration in pdf processor module

--- src/processors/pdf/__init__.py
+++ src/processors/pdf/__init__.py
@@ -2,7 +2,6 @@ from .pdf_processor import PdfProcessor
 from .o19_pdf_fetcher import O19PdfFetcher
 from .local_pdf_fetcher import LocalPdfFetcher
 
-__all__ = ['PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher']
 from .pdf_fetcher import PdfFetcher
 from .pdf_processor import PdfProcessor
 
