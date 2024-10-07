04ed1e453d2cba39521a49f3759390f7df0b0841
Michael Yingbull <myingbull@conestogac.on.ca>
2024-10-07 01:21:24 -0400

    chore: remove the hello module and all references to it from the codebase

--- src/processors/pdf/__init__.py
+++ src/processors/pdf/__init__.py
@@ -2,6 +2,5 @@ from .pdf_processor import PdfProcessor
 from .o19_pdf_fetcher import O19PdfFetcher
 from .local_pdf_fetcher import LocalPdfFetcher
 from .pdf_fetcher import PdfFetcher
-from .hello import hello
 
-__all__ = ['PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher', 'PdfFetcher', 'hello']
+__all__ = ['PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher', 'PdfFetcher']
