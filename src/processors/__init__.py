1b772a90594f415662bb9e39b57f2e4cc6bcfaf5
Michael Yingbull <myingbull@conestogac.on.ca>
2024-10-07 01:18:05 -0400

    fix: remove unnecessary references to nonexistent hello module in processors module

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,10 +1,1 @@
-from .document import DocumentProcessor
 from .document import DocumentProcessor, LocalFileProcessor, O19Processor
-from .pdf import PdfProcessor, O19PdfFetcher, LocalPdfFetcher
-from .workflow import WorkflowProcessor, WorkflowStepExecutor, WorkflowTaskManager, Workflow
-
-__all__ = [
-    'DocumentProcessor', 'LocalFileProcessor', 'O19Processor',
-    'PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher',
-    'WorkflowProcessor', 'WorkflowStepExecutor', 'WorkflowTaskManager', 'Workflow'
-]
--- src/processors/pdf/__init__.py
+++ src/processors/pdf/__init__.py
@@ -1,11 +1,8 @@
-from .pdf_fetcher import PdfFetcher
+from .pdf_processor import PdfProcessor
 from .o19_pdf_fetcher import O19PdfFetcher
 from .local_pdf_fetcher import LocalPdfFetcher
-from .pdf_processor import PdfProcessor
 
-__all__ = ['PdfFetcher', 'PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher']
-
-__all__ = ['PdfFetcher', 'PdfProcessor']
+__all__ = ['PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher']
 from .pdf_fetcher import PdfFetcher
 from .pdf_processor import PdfProcessor
 
