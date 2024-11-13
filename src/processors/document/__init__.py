e67fd8e280e2d41d9ff3dfeabe9402bd0fed38e6
Justin Joseph <justin.josephmkj@gmail.com>
2024-11-13 17:11:10 -0500

    Code Clean Up

--- src/processors/document/__init__.py
+++
@@ -1,5 +0,0 @@
-from .document_processor import DocumentProcessor
-from .local_file_processor import LocalFileProcessor
-from .o19_processor import O19Processor
-
-__all__ = ['DocumentProcessor', 'LocalFileProcessor', 'O19Processor']
--- src/processors/pdf/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pdf_processor import PdfProcessor
-from .o19_pdf_fetcher import O19PdfFetcher
-from .local_pdf_fetcher import LocalPdfFetcher
-from .pdf_fetcher import PdfFetcher
-
-__all__ = ['PdfProcessor', 'O19PdfFetcher', 'LocalPdfFetcher', 'PdfFetcher']
