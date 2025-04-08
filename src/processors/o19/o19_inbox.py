f64faa684ccc2ad8f0e4bfb035fc354401177098
justin joseph <justin.josephmkj@gmail.com>
2025-04-08 16:25:14 -0400

    Missing Import for timeout

--- src/processors/o19/o19_inbox.py
+++ src/processors/o19/o19_inbox.py
@@ -20,12 +20,9 @@
 # ***
 
 from datetime import datetime, timedelta
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 def get_document_processor_type(self):
 	"""
