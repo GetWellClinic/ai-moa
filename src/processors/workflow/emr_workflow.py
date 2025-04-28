591c4dfb654ba33b67725292152587a2e14c6f05
justin joseph <justin.josephmkj@gmail.com>
2025-04-28 13:14:33 -0400

    User-agent

--- src/processors/workflow/emr_workflow.py
+++ src/processors/workflow/emr_workflow.py
@@ -97,6 +97,7 @@ class Workflow:
 
         self.headers['Origin'] = self.origin_url
         self.headers['Referer'] = self.base_url
+        self.headers['User-Agent'] = config.get('emr.user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36')
 
         self.update_o19 = o19_updater.update_o19
         self.view_output = o19_updater.view_output
